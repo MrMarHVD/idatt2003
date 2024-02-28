e85ae0e7dbe04fe774109e68ec9f59d15ab992d6
Håvard Daleng <142524365+MrMarHVD@users.noreply.github.com>
2024-02-28 13:25:36 +0100

    Created placeholder ChaosGame class.

---
+++ src/main/java/edu/ntnu/stud/chaosgame/model/handling/ChaosGame.java
@@ -0,0 +1,31 @@
+package edu.ntnu.stud.chaosgame.model.handling;
+
+import edu.ntnu.stud.chaosgame.model.Vector2D;
+import java.util.Random;
+
+/**
+ * Class representing the chaos game.
+ */
+public class ChaosGame {
+
+  /**
+   * The canvas for this ChaosGame
+   */
+  private ChaosCanvas canvas;
+
+  /**
+   * The description for this ChaosGame
+   */
+  // private ChaosGameDescription description;
+
+  /**
+   * The current point in the chaos game.
+   */
+  private Vector2D currentPoint;
+
+  /**
+   * The random number generator for the chaos game.
+   */
+  public Random random;
+
+}
